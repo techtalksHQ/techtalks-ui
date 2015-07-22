461df0e62cd6a01ca027e98dda8b985aba8bad3a
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-07-22 16:09:07 +0200

    starting infrastructure

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: techtalks-ui
+# about: Encapsulates the UI for techtalks.xyz
+# version: 0.0.1
+# authors: Benjamin Kampmann, Anouk Ruhaak
+# url: https://github.com/techtalksHQ/techtalks-ui
+
+# stylesheet
+register_asset "stylesheets/main.scss"
+# register_asset "stylesheets/mobile.scss", :mobile
+
+after_initialize do
+
+end
